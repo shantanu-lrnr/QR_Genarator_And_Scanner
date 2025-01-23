037b0bcf2410e715c449b1e158b7c1e05e0de07e
shantanu <kanojiashantanu123@gmail.com>
2025-01-23 16:24:48 +0530

    first commit

---
+++ scanner/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from scanner.views import generate_qr, scan_qr
+urlpatterns = [
+    path('generate/', generate_qr, name='generate_qr'),
+    path('scan/', scan_qr, name='scan_qr'),
+]
